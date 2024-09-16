8c1e4b3a17c808169d9cbb9b1c12d030394f5f6f
Dan Heer <mndanh@gmail.com>
2024-09-16 16:06:11 -0500

    netlify 4

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'; // import path
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'), // Add this alias
