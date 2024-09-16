f605ea53e411ae91d5b504d72b243535cd49367f
Dan Heer <mndanh@gmail.com>
2024-09-16 16:02:09 -0500

    vite config file needed more info for images

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // import path
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'), // Add this alias
+    },
   },
-)
+});
