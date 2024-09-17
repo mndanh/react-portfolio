88a7793a1f33ab432a54e5a1ff452d30e90545e3
Dan Heer <mndanh@gmail.com>
2024-09-17 16:26:31 -0500

    fixed resume rendering

--- client/pages/resume.jsx
+++ client/pages/resume.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { testresume2 } from '../src/assets/pdf'
 
 const ResumePage = () => {
   return (
@@ -6,17 +7,14 @@ const ResumePage = () => {
       <h1>My Resume</h1>
       <div className="resume-container">
         <iframe 
-            src="https://docs.google.com/document/d/1eKff0mVj56yKyzZtmpj6T3dvJVFssABh7L7uxPhF858/pub?embedded=true"
-            // width="100%"
-            // height="600"
-            // frameBorder="0"
+            src={testresume2}
+            width="100%"
+            height="600"
+            frameBorder="0"
             title="Resume"
             allowFullScreen>
         </iframe>
     </div>
-        <a href="https://docs.google.com/document/d/1eKff0mVj56yKyzZtmpj6T3dvJVFssABh7L7uxPhF858/pub" download>
-            <button>Download Resume</button>
-        </a>
     </div>
     );
 };
--- client/src/assets/pdf/index.js
+++ client/src/assets/pdf/index.js
@@ -1,5 +1,7 @@
 import testresume from '../pdf/testresume.pdf';
+import testresume2 from '../pdf/testresume2.pdf';
 
 export {
     testresume,
+    testresume2,
 };
